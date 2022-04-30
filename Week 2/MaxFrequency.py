e32d9591825b83a9169c3398a3e0744c35d55b4f
Naboni <se.naboni.abebe@gmail.com>
2022-04-30 15:08:29 +0300

    Max Frequency

---
+++ Week 2/MaxFrequency.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        total = 0
+        left = 0
+        right = 0
+        result = 0
+        while (right < len(nums)):
+            total += nums[right]
+            if (nums[right] * (right - left + 1) > total + k):
+                total -= nums[left]    
+                left += 1
+                
+            result = max(result, right - left + 1)
+            right += 1
+        
+        return result
