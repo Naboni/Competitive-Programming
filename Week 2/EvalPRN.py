fdda9e8a7aefa7a8fde21175598795a13df6427d
Naboni <se.naboni.abebe@gmail.com>
2022-05-19 00:02:32 +0300

    Decode String and modification of others

--- Week 2/EvalPRN.py
+++ Week 2/EvalPRN.py
@@ -1,28 +1,24 @@
 import math
-class Solution:
-    def evalRPN(self, tokens: List[str]) -> int:
-        ops = ["+", "-", "*", "/"]
-        result = []
-        for i in range(len(tokens)):
-            if tokens[i] not in ops:
-                result.append(tokens[i])
+def evrpn(tokens):
+    stack = []
+    for i in range(len(tokens)):
+        if tokens[i] == '+':
+            stack.append(stack.pop()+stack.pop())
+        elif tokens[i] == '*':
+            stack.append(stack.pop()*stack.pop())
+        elif tokens[i] == '-':
+            stack.append(-1*(stack.pop()-stack.pop()))
+        elif tokens[i] == '/':
+            x = stack.pop()
+            res = stack.pop()/x
+            if res < 0:
+                stack.append(math.ceil(res))
             else:
-                if tokens[i] == "+":
-                    calc = int(result[-2]) + int(result[-1])
-                elif tokens[i] == "-":
-                    calc = int(result[-2]) - int(result[-1])
-                elif tokens[i] == "/":
-                    if int(result[-2]) / int(result[-1]) < 0:
-                        calc = math.ceil(int(result[-2]) / int(result[-1]))
-                    else:
-                        calc = int(result[-2]) // int(result[-1])
-                elif tokens[i] == "*":
-                    calc = int(result[-2]) * int(result[-1])
-                result.pop()
-                result.pop()
-                result.append(calc)
-        return result[0]
+                stack.append(math.floor(res))
+        else:
+            stack.append(int(tokens[i]))
+    return stack[0]
 
-print(evalRPN(["2","1","+","3","*"]))
-print(evalRPN(["4","13","5","/","+"]))
-print(evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"]))
+print(evrpn(["2","1","+","3","*"]))
+print(evrpn(["4","13","5","/","+"]))
+print(evrpn(["10","6","9","3","+","-11","*","/","*","17","+","5","+"]))
--- Week 2/MinStack.py
+++ Week 2/MinStack.py
@@ -2,14 +2,19 @@ class MinStack:
 
     def __init__(self):
         self.stack = []
-        
-    # pushes the element val onto the stack
+        self.min = []
+    # pushes the element val onto the end of the stack
     def push(self, val: int) -> None:
+        if self.stack and self.min[-1] < val:
+            self.min.append(self.min[-1])
+        else:
+            self.min.append(val)
         self.stack.append(val)
     
     # removes the element on the top of the stack
     def pop(self) -> None:
         self.stack.pop()
+        self.min.pop()
 
     # gets the top element of the stack
     def top(self) -> int:
@@ -17,7 +22,7 @@ class MinStack:
 
     # retrieves the minimum element in the stack
     def getMin(self) -> int:
-        return min(self.stack)
+        return self.min[-1]
 
 
 # Your MinStack object will be instantiated and called as such:
