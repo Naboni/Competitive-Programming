460b1d2de671ad93ad0e2cdd1cc96d6f8674e946
Naboni <se.naboni.abebe@gmail.com>
2022-05-18 11:26:54 +0300

    Amortized MyQueue

--- Week 2/MyQueue.py
+++ Week 2/MyQueue.py
@@ -1,23 +1,32 @@
 class MyQueue:
 
     def __init__(self):
-        self.queue = []
+        self.stack1 = []
+        self.stack2 = []
+        self.front = None
         
     # pushes to the back of the queue
     def push(self, x: int) -> None:
-        self.queue.append(x)
+        if not self.stack1:
+            self.front = x
+        self.stack1.append(x)
 
     # removes from the front of the queue and returns it
     def pop(self) -> int:
-        return self.queue.pop(0)
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2.pop()
 
     # returns element at the front of the queue
     def peek(self) -> int:
-        return self.queue[0]
+        if self.stack2:
+            return self.stack2[-1]
+        return self.front
     
     # true if empty and false if not 
     def empty(self) -> bool:
-        return True if len(self.queue)==0 else False
+        return (not self.stack1 and not self.stack2)
 
 
 # Your MyQueue object will be instantiated and called as such:
