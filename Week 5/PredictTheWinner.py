035380fd005bb0708f659fd9458e7e1a60635fd1
Naboni <se.naboni.abebe@gmail.com>
2022-05-24 11:36:05 +0300

    Update PredictTheWinner.py

--- Week 5/PredictTheWinner.py
+++ Week 5/PredictTheWinner.py
@@ -2,13 +2,13 @@ class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
         if len(nums) == 1:
             return True
-
-        def predict(nums):
-            if len(nums) == 1:
-                return nums[0]
-            return max(nums[0]-predict(nums[1:]), 
-                       nums[-1]-predict(nums[:-1]))
-        score = predict(nums)
+        score = self.play(nums)
         return score >= 0
+    
+    def play(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        return max(nums[0] - self.play(nums[1:]), nums[-1] - self.play(nums[:-1]))
+
 print(obj.PredictTheWinner([1,5,2]))
 print(obj.PredictTheWinner([1,5,233,7]))
