7734b6b9c0789fe1e1aa64c25b62a16c5ea4d69b
Naboni <se.naboni.abebe@gmail.com>
2022-05-30 16:53:23 +0300

    Reverse Nodes in k-Group

--- Week 3/SpiralOrder.py
+++ Week 3/SpiralOrder.py
@@ -1,28 +1,24 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        top  = 0
-        bottom = len(matrix)-1
-        left = 0
-        right = len(matrix[0])-1
-        notDone = True
-        spiral = []
-        while top <= bottom and left <= right and notDone:
-            for i in range(left, right+1):
-                spiral.append(matrix[top][i])
-            if top < bottom:
-                for j in range(top+1, bottom+1):
-                    spiral.append(matrix[j][right])
-                for k in range(right-1, left-1,-1):
-                    spiral.append(matrix[bottom][k])
-            else:
-                notDone = False
-            if left < right:
-                for l in range(bottom-1, top, -1):
-                    spiral.append(matrix[l][left])
-            else:
-                notDone = True
-            top += 1
-            bottom -= 1
-            left += 1
-            right -= 1
-        return spiral
+        left = top = 0
+        right = len(matrix[0])
+        bottom = len(matrix)
+        output = []
+        
+        while left<right and top<bottom:
+            for i in range(left,right):
+                output.append(matrix[top][i])
+            top+=1
+            for i in range(top,bottom):
+                output.append(matrix[i][right-1])
+            right-=1
+            if not (left<right and top<bottom):
+                break
+            for i in range(right-1,left-1,-1):
+                output.append(matrix[bottom-1][i])
+            bottom-=1
+            for i in range(bottom-1,top-1,-1):
+                output.append(matrix[i][left])
+            left+=1
+        return output
+            
