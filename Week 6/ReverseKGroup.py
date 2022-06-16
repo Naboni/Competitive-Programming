799340dce9974b63b751d71d9aedd5a036641d2c
Naboni <se.naboni.abebe@gmail.com>
2022-06-16 13:35:03 +0300

    Update ReverseKGroup.py

--- Week 6/ReverseKGroup.py
+++ Week 6/ReverseKGroup.py
@@ -26,4 +26,22 @@ class Solution:
             count -= k
             
         return dummy.next
+    # recursively
+    def reverseKGroupRecursive(self, head: ListNode, k: int) -> ListNode:   
+        # check if it needs to be reversed
+        curr = head
+        for _ in range(k):
+            if not curr:
+                return head
+            curr = curr.next
+        # standard reversing
+        prev, curr = None, head 
+        for _ in range(k):
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        # after reverse head is now tail of the group
+        head.next = self.reverseKGroup(curr, k)
+        return prev
         
