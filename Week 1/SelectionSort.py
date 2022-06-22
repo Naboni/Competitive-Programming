f55fc02d892761b3d9a46352bcf30865a46d4b31
Naboni <se.naboni.abebe@gmail.com>
2022-06-22 10:09:36 +0300

    Update SelectionSort.py

--- Week 1/SelectionSort.py
+++ Week 1/SelectionSort.py
@@ -1,13 +1,15 @@
+import math
 class Solution: 
     def select(self, arr, i):
-        return arr[i]
+        min_index = i
+        for j in range(i, len(arr)-1):
+            if arr[j+1] < arr[min_index]:
+                min_index = j+1
+        return min_index
+            
     
     def selectionSort(self, arr,n):
-        for i in range(n-1):
-            minimum = i
-            for j in range(i+1, n):
-                if arr[j] < arr[minimum]:
-                    minimum = j
-            if minimum != i:
-                arr[i], arr[minimum] = arr[minimum], arr[i]
+        for i in range(n):
+            min_index = self.select(arr, i)
+            arr[i], arr[min_index] = arr[min_index], arr[i]
         return arr
