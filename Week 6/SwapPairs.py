e941e1de0aca6c061de3a6e4dc573d6fc68a46ad
Naboni <se.naboni.abebe@gmail.com>
2022-06-28 15:47:05 +0300

    Update SwapPairs.py

--- Week 6/SwapPairs.py
+++ Week 6/SwapPairs.py
@@ -4,8 +4,22 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    # Iterative
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head and head.next:
-            head.val, head.next.val = head.next.val, head.val
-            self.swapPairs(head.next.next)
-        return head
+        dummy = prev = ListNode(0, head)
+        while head and head.next:
+            temp = head.next
+            head.next = temp.next
+            temp.next = head
+            prev.next = temp
+            prev = temp.next
+            head = head.next
+        return dummy.next
+    # Recursive
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return head
+        temp = head.next
+        head.next = self.swapPairs(temp.next)
+        temp.next = head
+        return temp
