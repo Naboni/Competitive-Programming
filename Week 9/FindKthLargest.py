a35667ba4a47e5ed197c19212c89cd54f5837e6a
Naboni <se.naboni.abebe@gmail.com>
2022-08-03 15:58:00 +0300

    Update FindKthLargest.py

--- Week 9/FindKthLargest.py
+++ Week 9/FindKthLargest.py
@@ -1,16 +1,8 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        # easy way
-        # nums.sort()
-        # return nums[len(nums)-k]
-        if not nums or not k or k < 0:
-            return None
-        minheap = []
+        heap = []
         for num in nums:
-            if len(minheap) < k:
-                heapq.heappush(minheap, num)
-            else:
-                if num > minheap[0]:
-                    heapq.heappop(minheap)
-                    heapq.heappush(minheap, num)
-        return minheap[0]
+            heappush(heap, num)
+            if len(heap) > k:
+                heappop(heap)
+        return heap[0]
