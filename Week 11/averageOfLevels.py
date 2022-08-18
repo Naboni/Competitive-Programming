1c6a4999ae3e3b33f2e1a2713390d68a584bff08
Naboni <se.naboni.abebe@gmail.com>
2022-08-19 01:32:21 +0300

    Create averageOfLevels.py

---
+++ Week 11/averageOfLevels.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        if not root: return 0
+        q = deque([root])
+        res = []
+        while q:
+            levelSum = 0
+            n = len(q)
+            for _ in range(n):
+                node = q.popleft()
+                levelSum += node.val
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            res.append(levelSum/n)
+        return res
