eb7b286cae2cf85e9ee920814721c073dff5b99b
Naboni <se.naboni.abebe@gmail.com>
2022-11-23 16:13:23 +0300

    subset dp

---
+++ subset_sum_diff_min_abs_value.py
@@ -0,0 +1,19 @@
+arr = list(map(int, input().split()))
+n = len(arr)
+tot = sum(arr)
+memo = {}
+# def topdown(i, s1, s2):
+#     if i >= n: return abs(s1 - s2)
+#     if (i, s1, s2) in memo: return memo[(i, s1, s2)]
+#     memo[(i, s1, s2)] = min(topdown(i+1,s1 + arr[i], s2), topdown(i+1,s1, s2 + arr[i]))
+#     return memo[(i, s1, s2)]
+
+# print(topdown(0, 0, 0))
+
+def topdown(i, s1):
+    if i >= n: return abs((tot - s1) - s1)
+    if (i, s1) in memo: return memo[(i, s1)]
+    memo[(i, s1)] = min(topdown(i+1,s1 + arr[i]), topdown(i+1,s1))
+    return memo[(i, s1)]
+
+print(topdown(0, 0))
